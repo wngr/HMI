ad63773616002d93d3322bb7b8d66917bfa5f438
Thomas Frei <mail@thomasfrei.de>
2014-10-15 17:36:06 +0200

    Test Class with an Event-Listener as a method. Inherits EventEmitter from events
    Good working example

--- models/modeltest.js
+++ models/modeltest.js
@@ -5,27 +5,82 @@
  * @date   2014-10-12 
  */
 
+/*
+ * Testing for Events and Classes.
+ * 1. Define Class
+ * 2. Inhreit Event handlers
+ * 3. New Class-Object
+ * 4. Register Event-Listeners // Do the Event-litsener in the class-constructor!!! (see line 30)
+ * 5. Start using the class
+ */
 var util = require("util");
 var events = require("events");
 
-
-function MyStream() {
+/*
+ * Define Class
+ */
+MyFamily = (function() {
+  function MyFamily() {
+    this.name = 'Max';
+    this.lastname = 'Mustermann';
+    this.age = '0';
+    
+    this.reactions();
+    
     events.EventEmitter.call(this);
-}
+  }
 
-util.inherits(MyStream, events.EventEmitter);
+  MyFamily.prototype = {
+    constructor: MyFamily,
+    giveBirth: function(name, lastname) {
+      this.name = name;
+      this.lastname = lastname;
+      this.age = 0
+      
+      msg = 'Heureka! A child was born. It may carry the name: ' + name + ' ' + lastname;
+      console.log(msg);
+      this.emit('born', name + ' ' + lastname );
+    },
+    die: function(){
+      console.log(this.field + 2);
+    },
+    reactions: function(){
+      this.on('born', function(){ console.log('The Child is out!'); });
+    }
+  };
 
-MyStream.prototype.write = function(data) {
-    this.emit("data", data);
-}
+  return MyFamily;
+}());
 
-var stream = new MyStream();
+// 2.
+MyFamily.prototype.__proto__ = events.EventEmitter.prototype;
 
-console.log(stream instanceof events.EventEmitter); // true
-console.log(MyStream.super_ === events.EventEmitter); // true
+//3.
+Frei = new MyFamily();
 
-stream.on("data", function(data) {
-    console.log('Received data: "' + data + '"');
-});
+//4.
+Frei.on('born', function(msg){ console.log('I heard a ' + msg + ' was born!'); } );
 
-stream.write("It works!"); // Received data: "It works!"
+//5.
+Frei.giveBirth('Carina', 'Frei');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
