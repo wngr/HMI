f8727da57fb4621763395c5b29e6e236142f7ccc
Thomas Frei <mail@thomasfrei.de>
2014-10-28 17:41:12 +0100

    JSCS and Mocha out of base directory to start

--- test/testTest.js
+++ test/testTest.js
@@ -1,21 +1,55 @@
+/**
+ * Configuration
+ */
+GLOBAL.CONFIG = require('./../config.js');
 
+/**
+ * Node-Module dependencies.
+ * https://www.digitalocean.com/community/tutorials/how-to-install-express-a-node-js-framework-and-set-up-socket-io-on-a-vps
+ */
+var path = require('path'),
+  express = require('express'),
+  app = express(),
+  http = require('http'),
+  server = http.createServer(app),
+  should = require('should'); // For Testing
 
-describe('MyClass', function () {
+GLOBAL.IO = require('socket.io').listen(server);
+GLOBAL._ = require('underscore');
+GLOBAL.md5 = require('MD5');
 
-  describe('addOne()', function () {
+describe('opcuaInstance', function() {
 
-    it('should correctly add one to the given number', function () {
+  describe('readArray()', function() {
+
+    var opcuaSession1 = require('./../models/opcuaInstance').server('opc.tcp://localhost:4334/');
+    it('should read an Array of nodes out of a node-opcua server', function() {
+      opcuaSession1.on('readArrayFinished', function(data) {
+        console.log('=======================================================');
+        console.log('=======================================================');
+        console.log(JSON.stringify(data, null, 1));
+      });
+
+      opcuaSession1.on('ready', function() {
+        var NodesToRead = ['MI5.Module1101.Output.SkillOutput.SkillOutput0.Busy',
+                           'MI5.Module1101.Output.SkillOutput.SkillOutput0.Ready',
+                           'MI5.Module1101.Output.SkillOutput.SkillOutput0.Done'];
+        opcuaSession1.readArray(NodesToRead);
+
+        opcuaSession1.readArray(opcuaSession1.nodeArrayParameterOutputSingle('MI5.Module1101.Output.SkillOutput.SkillOutput0.ParameterOutput', 0));
+        opcuaSession1.readArray(opcuaSession1.nodeArraySkillOutputSingle('MI5.Module1101.Output.SkillOutput', 0));
+      });
+
+    });
+
+  });
+
+  describe('subtractOne()', function() {
+
+    it('should correctly subtract one from the given number', function() {
       // assertions here
-    })
+    });
 
-  })
+  });
 
-  describe('subtractOne()', function () {
-
-    it('should correctly subtract one from the given number', function () {
-      // assertions here
-    })
-
-  })
-
-})
+});
