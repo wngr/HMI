ac38c97966fe9818a19ca9e92e3be35d86c29b8d
Thomas Frei <mail@thomasfrei.de>
2014-11-05 10:41:15 +0100

    Before Array redesign structure:
    SkillOutput.SkillOutput0 - SkillOutput[0]

--- config.js
+++ config.js
@@ -30,3 +30,28 @@ exports.taskId = 1;
  * MessageFeed
  */
 exports.messageFeedUrl = 'opc.tcp://192.168.175.230:4840';
+
+function ModuleIdEndpointUrl(moduleId){
+  var endpointUrl = '';
+  switch (moduleId) {
+    case 1101:
+    case 1102:
+    case 1103:
+      endpointUrl = "localhost";
+      break;
+    case 2001:
+    case 2002:
+    case 2003:
+      endpointUrl = "192.168.175.210:4840";
+      break;
+    case 2401:
+    case 2402:
+      endpointUrl = "192.168.175.213:4840";
+      break;
+    default:
+      console.log('there is no correspondent IP:Port for the moduleid:', moduleId);
+      return false;
+  }
+  return 'opc.tcp://'+endpointUrl;
+}
+exports.ModuleIdToEndpointUrl = ModuleIdEndpointUrl;
