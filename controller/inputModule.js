8ac827b62a8ad4b3013074dc3b7fdc646499d419
Thomas Frei <mail@thomasfrei.de>
2014-11-18 12:03:17 +0100

    Input/OutputModule ModuleInterface working! Nice ;-)

--- controller/inputModule.js
+++ controller/inputModule.js
@@ -1,20 +1,34 @@
 /**
- * Hand Module Controller
+ * Input Module Controller
  */
 
 function index(req, res) {
   var jadeData = new Object;
 
   var interface = require('./../models/simpleModuleInterface');
-  interface.getInput(function(err, mi5object) {
-    if (err) {
-      console.log('ERR - Error in inputModule', err);
-      return 0;
-    }
-    jadeData.input = mi5object;
+  async.series([ function(callback) {
+    interface.getInput(function(err, mi5object) {
+      if (err) {
+        console.log('ERR - Error in getInput', err);
+        return 0;
+      }
+      jadeData.input = mi5object;
+      callback(err);
+    })
+  }, function(callback) {
+    interface.getOutput(function(err, mi5object) {
+      if (err) {
+        console.log('ERR - Error at getOutput', err);
+        return 0;
+      }
+      jadeData.output = mi5object;
+      callback(err);
+    });
+  }, function(callback) {
     console.log(JSON.stringify(jadeData, null, 1));
     res.render('sbadmin2/input_module_index', jadeData);
-  })
+    res.end();
+  } ]);
 
 }
 exports.index = index;
